b7c4e63e16d4991d555df1105671e7a522cbacf4
jenmawe <jenmawe@gmail.com>
2022-09-26 13:13:48 -0400

    Update file
    accounted for property name change

--- orphanBibs.sql
+++ orphanBibs.sql
@@ -3,10 +3,13 @@ SELECT
 	rt.id AS srs_bib_uuid,
 	rt.state AS srs_bib_state,
 	rt.record_type AS srs_bib_record_type,
+	rt.external_ids_holder__instance_id AS instance_uuid,
+	rt.external_ids_holder__instance_hrid AS instance_hrid,
 	rt.metadata__created_date,
 	rt.metadata__updated_date,
 	rt.metadata__created_by_user_id,
 	rt.metadata__updated_by_user_id
 FROM 
 	source_record.records__t rt
-WHERE rt.instance_id IS NULL; 
+WHERE
+	rt.external_ids_holder__instance_id IS NULL ; 
